e424df435f987e5d1ba72f2fb544b3e3eac0639f
Dzenoo <frontprogramer@gmail.com>
2023-07-18 11:30:45 +0200

    Adding validate

---
+++ hooks/useValidation.tsx
@@ -0,0 +1,63 @@
+import {
+  ActionType,
+  ActionTypes,
+  ValidatorState,
+  ValidatorTypes,
+} from "@/types/Validator";
+import { validate } from "@/utility/validators";
+import React, { useReducer } from "react";
+
+const initialState: ValidatorState = {
+  value: "",
+  isValid: false,
+  isTouched: false,
+};
+
+const reducer = (state: ValidatorState, action: ActionTypes) => {
+  switch (action.type) {
+    case ActionType.INPUT_CHANGE: {
+      return {
+        ...state,
+        value: action.payload,
+        isValid: validate(action.payload, action.validators),
+      };
+    }
+    case ActionType.INPUT_BLUR: {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export const useValidation = (validators: ValidatorTypes[]) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: ActionType.INPUT_CHANGE,
+      payload: e.target.value,
+      validators: validators,
+    });
+  };
+
+  const onBlurHandler = () => {
+    dispatch({
+      type: ActionType.INPUT_BLUR,
+      payload: "",
+      validators: [],
+    });
+  };
+
+  return {
+    value: state.value,
+    isTouched: state.isTouched,
+    isValid: state.isValid,
+    onChangeHandler,
+    onBlurHandler,
+  };
+};
--- types/Validator.tsx
+++ types/Validator.tsx
@@ -1,5 +1,22 @@
+export enum ActionType {
+  INPUT_CHANGE = "INPUT_CHANGE",
+  INPUT_BLUR = "INPUT_BLUR",
+}
+
 export type ValidatorTypes = {
   type: string;
   password?: string;
   val?: number;
+};
+
+export type ActionTypes = {
+  type: ActionType;
+  payload: string;
+  validators: ValidatorTypes[];
+};
+
+export type ValidatorState = {
+  value: string;
+  isValid: boolean;
+  isTouched: boolean;
 };
