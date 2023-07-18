7ec22ffaf3e1131b4803a64bbe06742bd2982a41
Dzenoo <frontprogramer@gmail.com>
2023-07-18 10:02:31 +0200

    Commenting

--- data/data.config.ts
+++ data/data.config.ts
@@ -3,7 +3,6 @@ const categoryFilters = [
   { label: "Technology", value: "technology" },
   { label: "Business", value: "business" },
   { label: "Arts", value: "arts" },
-  // Add more categories as needed
 ];
 
 // Price Range Filter
@@ -11,7 +10,6 @@ const priceRangeFilters = [
   { label: "Free", value: "free" },
   { label: "$0 - $50", value: "0-50" },
   { label: "$50 - $100", value: "50-100" },
-  // Add more price ranges as needed
 ];
 
 // Duration Filter
@@ -19,7 +17,6 @@ const durationFilters = [
   { label: "Short-term", value: "short" },
   { label: "Medium-term", value: "medium" },
   { label: "Long-term", value: "long" },
-  // Add more duration options as needed
 ];
 
 // Skill Level Filter
@@ -27,7 +24,6 @@ const skillLevelFilters = [
   { label: "Beginner", value: "beginner" },
   { label: "Intermediate", value: "intermediate" },
   { label: "Advanced", value: "advanced" },
-  // Add more skill levels as needed
 ];
 
 // Language Filter
@@ -35,7 +31,6 @@ const languageFilters = [
   { label: "English", value: "english" },
   { label: "Spanish", value: "spanish" },
   { label: "French", value: "french" },
-  // Add more languages as needed
 ];
 
 // Rating and Reviews Filter
@@ -43,7 +38,6 @@ const ratingFilters = [
   { label: "4 stars and above", value: "4+" },
   { label: "3 stars and above", value: "3+" },
   { label: "2 stars and above", value: "2+" },
-  // Add more rating options as needed
 ];
 
 // Certification Filter
@@ -55,7 +49,6 @@ const certificationFilter = [
 const instructorFilters = [
   { label: "John Doe", value: "john-doe" },
   { label: "Jane Smith", value: "jane-smith" },
-  // Add more instructors as needed
 ];
 
 // Format Filter
@@ -63,26 +56,22 @@ const formatFilters = [
   { label: "Video", value: "video" },
   { label: "Text", value: "text" },
   { label: "Interactive", value: "interactive" },
-  // Add more format options as needed
 ];
 
 // Availability Filter
 const availabilityFilters = [
   { label: "On-Demand", value: "ondemand" },
   { label: "Next Month", value: "next-month" },
-  // Add more availability options as needed
 ];
 
 // Popularity Filter
 const popularityFilters = [
   { label: "Most Enrolled", value: "most-enrolled" },
   { label: "Highest Rated", value: "highest-rated" },
-  // Add more popularity options as needed
 ];
 
 // Course Type Filter
 const courseTypeFilters = [
   { label: "Self-Paced", value: "self-paced" },
   { label: "Instructor-Led", value: "instructor-led" },
-  // Add more course types as needed
 ];
