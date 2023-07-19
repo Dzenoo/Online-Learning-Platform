714c8ca340fb7ea82d3ea36aac39b6ad4205d223
Dzenoo <frontprogramer@gmail.com>
2023-07-19 12:21:38 +0200

    Adding input

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,7 +1,18 @@
+import { CourseList, CoursesFilter, CoursesInput } from "@/components/courses";
 import React from "react";
 
 const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+  return (
+    <section className="p-4 mt-2 flex justify-center items-stretch flex-wrap gap-2">
+      <div className="p-4 basis-1/2 border">
+        <CoursesInput />
+        <CourseList />
+      </div>
+      <div className="p-4 basis-1/6 border">
+        <CoursesFilter />
+      </div>
+    </section>
+  );
 };
 
 export default CoursesPage;
--- components/courses/filter/CoursesInput.tsx
+++ components/courses/filter/CoursesInput.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import Input from "@/components/shared/form/Input";
+import { useValidation } from "@/hooks/useValidation";
+import { VALIDATOR_REQUIRE } from "@/utility/validators";
 import React from "react";
 
 const CoursesInput = () => {
-  return <div>CoursesInput</div>;
+  const searchInput = useValidation([VALIDATOR_REQUIRE()]);
+
+  return (
+    <div>
+      <Input
+        type="input"
+        error={false}
+        helperText={"Enter Valid Search Text"}
+        label={"Search Courses"}
+        placeholder={"e.g JavaScript for beginners"}
+        value={searchInput.value}
+        onChange={searchInput.onChangeHandler}
+        onBlur={searchInput.onBlurHandler}
+        id={"search_input"}
+      />
+    </div>
+  );
 };
 
 export default CoursesInput;
