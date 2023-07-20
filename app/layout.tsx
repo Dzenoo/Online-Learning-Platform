0d8e778baae1119cc742c3617140b66eba07e3f6
Dzenoo <frontprogramer@gmail.com>
2023-07-20 14:04:43 +0200

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer, MainNavigation } from "@/components/landing";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainNavigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
