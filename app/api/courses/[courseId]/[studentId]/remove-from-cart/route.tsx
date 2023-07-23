6563d69ab066532857c0218c9ddd37e690ade6d4
Dzenoo <frontprogramer@gmail.com>
2023-07-23 23:25:06 +0200

    remove from cart

---
+++ app/api/courses/[courseId]/[studentId]/remove-from-cart/route.tsx
@@ -0,0 +1,35 @@
+import { connectToDB } from "@/library/database";
+import Course from "@/models/course";
+import Student from "@/models/student";
+import { responseJson, responseMessage } from "@/utility/helpers";
+
+export const PATCH = async (
+  request: Request,
+  { params }: { params: { courseId: string; studentId: string } }
+) => {
+  try {
+    await connectToDB();
+
+    const course = await Course.findById(params.courseId);
+    const student = await Student.findById(params.studentId);
+
+    if (!course) {
+      return responseMessage("Could not find course", 404);
+    }
+
+    if (!student) {
+      return responseMessage("Could not find student", 404);
+    }
+
+    if (student.cart.items.includes(course._id)) {
+      student.cart.items.pull(course._id);
+      student.cart.totalAmount -= course.price;
+    }
+
+    await student.save();
+    return responseMessage("Course added to cart ", 201);
+  } catch (error) {
+    console.log(error);
+    return responseMessage("Internal server error", 500);
+  }
+};
