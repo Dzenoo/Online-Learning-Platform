d7fdbf6709c1adea457ebf7029dd93704141f04f
Dzenoo <frontprogramer@gmail.com>
2023-07-26 19:45:54 +0200

    adding custom hook

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,30 +1,32 @@
 "use client";
 
 import Signup from "@/components/auth/Signup";
+import { usePostHttp } from "@/hooks/usePostHttp";
 import { RegisterData, SignupType } from "@/types/auth/SignupTypes";
 
 const SignupPage = () => {
+  const { sendRequest, isLoading, message } = usePostHttp();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (message) {
+    return <p>{message}</p>;
+  }
+
   async function registerUser(
     data: RegisterData,
     type: SignupType.Instructor | SignupType.Student
   ) {
-    if (data) {
-      const response = await fetch("/api/auth/signup", {
-        method: "POST",
-        body: JSON.stringify({
-          first_name: data.first_name,
-          last_name: data.last_name,
-          email: data.email,
-          password: data.password,
-          type: type,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-      const responseData = await response.json();
-      console.log(responseData);
-    } else {
-      return;
-    }
+    const signupData = {
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: data.password,
+      type: type,
+    };
+    const response = await sendRequest("POST", "/api/auth/signup", signupData);
   }
 
   return (
---
+++ hooks/usePostHttp.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
+export const usePostHttp = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+
+  async function sendRequest(method: string, url: string, data: {}) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      });
+      const responseData = await response.json();
+
+      if (response.ok) {
+        setIsLoading(false);
+        responseData.message && setMessage(responseData.message);
+        return responseData;
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log("Something went wrong");
+    }
+  }
+
+  return { sendRequest, isLoading, message };
+};
