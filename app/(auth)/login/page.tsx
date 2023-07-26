a4e599b093b7065de5ed35d8070f01f20805578d
Dzenoo <frontprogramer@gmail.com>
2023-07-26 19:47:48 +0200

    login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import Login from "@/components/auth/Login";
+import { usePostHttp } from "@/hooks/usePostHttp";
 import { LoginData, LoginType } from "@/types/auth/LoginTypes";
 
 const LoginPage = () => {
-  function loginUser(
+  const { sendRequest, isLoading, message } = usePostHttp();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (message) {
+    return <p>{message}</p>;
+  }
+
+  async function loginUser(
     data: LoginData,
     type: LoginType.Instructor | LoginType.Student
   ) {
-    if (type === LoginType.Instructor) {
-      console.log("Instructor");
-    } else {
-      console.log("Student");
-    }
+    const loginData = {
+      email: data.email,
+      password: data.password,
+      type: type,
+    };
+    const response = await sendRequest("POST", "/api/auth/login", loginData);
   }
 
   return (
